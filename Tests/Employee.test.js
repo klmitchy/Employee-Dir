19ef0bdf56ae85fa70508f22560e10436ddf92a7
klmitchy <karenapolis@gmail.com>
2022-04-21 10:18:18 -0500

    test script change

--- Tests/Employee.test.js
+++ Tests/Employee.test.js
@@ -1,12 +1,7 @@
 const Test = require('jest');
-const Employee = require('./Employee.js');
+const Employee = require('../Lib/Employee.js');
 const Employee = NewEmployee ("Bob", "123", "bob@hotmail.com");
 
-test ("Make sure constructor will grab values for new employee OBJECT",()=>{
-    expect(Employee.Name).toBe("Bob");
-    expect(Employee.ID).toBe("123");
-    expect(Employee.Email).toBe("bob@hotmail.com");
-});
 
 test ("Make sure I can get the name value from the getName METHOD",()=>{
     expect(Employee.getName().toBe("Bob"));
@@ -22,6 +17,12 @@ test ("Make sure the Email value is grabbed from the getEmail METHOD",()=>{
 
 test ("MAke sure the role is grabbed from the getRole METHOD",()=>{
     expect(Employee.getRole().toBe("Employee"));
+});
+
+test ("Make sure constructor will grab values for new employee OBJECT",()=>{
+    expect(Employee.Name).toBe("Bob");
+    expect(Employee.ID).toBe("123");
+    expect(Employee.Email).toBe("bob@hotmail.com");
 });
 
 module.exports = Employee
