124ab39605387f9aa9e046528f1b4194ab5b38fa
klmitchy <karenapolis@gmail.com>
2022-04-25 22:13:18 -0500

    several small changes to keep trying

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const promptMenu = () => {
                 break;
             case "Team Complete":
                 buildTeam();
-                break;
+               // break;
         }
     });
 };
